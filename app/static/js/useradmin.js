82a04e7599e38c0fb95fb28b605fd1a6c82d9b0b
enayetn <enayetn@gmail.com>
2015-11-18 22:56:20 -0600

    User Admin stuff

--- app/static/js/useradmin.js
+++ app/static/js/useradmin.js
@@ -5,9 +5,26 @@ define([ 'ractive', 'rv!../ractive/user-admin',  'jquery', 'dashglobals'], funct
     var userAdmin = new Ractive({
       el: 'userAdmin',
       data: {
+      	"users":{}
       },
     template: html
     });
+
+    function getUsers() {
+		$.ajax({
+			type: "GET",
+			url: "./api/v1.0/getUsers",
+			contentType : 'application/json',
+			dataType: "json",
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + apikey));
+			},
+			success: function(json) {
+				tableHistoryThree.set("users",json.response);
+				//hideAnimation("loading-screen");
+			}
+		});
+	}
 
     return userAdmin;
 
